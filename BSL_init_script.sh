28fbd4afc91f946310d5472ef012eaa919c2d0cc
nikola2444 <krajka95@gmail.com>
2020-12-08 12:03:35 +0100

    32 bit app repaired

--- BSL_init_script.sh
+++ BSL_init_script.sh
@@ -6,10 +6,9 @@ chmod a+x BSL_64bit_executable/BSL
 chmod a+x BSL_32bit_executable/BSL
 
 chmod 777 /dev/leds
-echo 0x00000000 > /dev/leds
 touch /dev/buttons
 chmod 777 /dev/buttons
-echo 0x0000 > /dev/buttons
+echo 0b0000 > /dev/buttons
 touch /dev/switches
 chmod 777 /dev/switches
-echo 0x0000 > /dev/switches
+echo 0b0000 > /dev/switches
